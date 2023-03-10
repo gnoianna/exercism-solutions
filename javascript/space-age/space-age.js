7c28021d006f70b0b972b0aac2b3e93a8d799c19
Anna Gnoińska <ania.gnoinska@gmail.com>
2023-03-10 13:30:29 +0100

    finish javascript space-age

---
+++ javascript/space-age/space-age.js
@@ -0,0 +1,23 @@
+//
+// This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const EARTH_DAYS_TO_SECONDS = 60 * 60 * 24 * 365.25;
+
+export const age = (planet, secondsOld) => {
+  return Number(
+    (secondsOld / (planets[planet] * EARTH_DAYS_TO_SECONDS)).toFixed(2)
+  );
+};
+
+const planets = {
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1.0,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132,
+};
